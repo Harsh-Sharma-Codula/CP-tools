418909590aa580eced0582cd7bedecdcd683674c
Harsh-Sharma-Codula <74403636+Harsh-Sharma-Codula@users.noreply.github.com>
2020-11-13 14:46:54 +0530

    Create Parse.py
    This file can be used to parse input and output from many CP sites.
    example- Codeforces , Codechef.

---
+++ Parse.py
@@ -0,0 +1,47 @@
+#Author-Harsh Sharma
+#codeforces-id:Codula
+#codechef-id:Codula1
+#Atcoder-id:Codula
+
+#This script will Parse The input and Output
+#USE:
+#python3 parse.py <Folder-Name> <Problem-Name>
+#List 7175 as a port in competitive companion.
+#Go to any problem and press the comcompetitive companion icon.
+
+
+
+from flask import Flask , request , redirect
+import json , subprocess
+import datetime
+import os
+import sys
+
+PORT = 7175
+
+def createNewTask(data):
+	taskData = json.loads(data.decode())
+		# print(subprocess.check_output(['subl' , os.path.join(baseContestPath , contestName , taskName + '.cpp')]))
+	with open("C:\\Users\\Harsh\\Documents\\contest\\cache.json","wb") as cache:
+		cache.write(json.dumps(taskData).encode())
+	with open("C:\\Users\\Harsh\\Documents\\contest\\cache.json") as cache:	
+		Final=json.load(cache)
+	a=1
+	for io in Final["tests"]:
+		inp=open("C:\\Users\\Harsh\\Documents\\contest\\{}\\{}\\in{}".format(sys.argv[1],sys.argv[2],a),"w")	
+		inp.write(io["input"])
+		inp.close()
+		out=open("C:\\Users\\Harsh\\Documents\\contest\\{}\\{}\\out{}".format(sys.argv[1],sys.argv[2],a),"w")	
+		out.write(io["output"])
+		out.close()
+		a+=1
+	
+
+app = Flask(__name__)
+
+@app.route('/' , methods = ['POST'])
+def getData():
+	createNewTask(request.data)
+	return redirect('/')
+
+app.run(port=PORT)
